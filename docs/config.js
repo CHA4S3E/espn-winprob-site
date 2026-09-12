e2776057a16a45ff2e528a1f8b4c47cfee28b1d2
CHA4S3E <103005524+CHA4S3E@users.noreply.github.com>
2026-09-12 15:45:50 -0400

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -2,4 +2,4 @@ window.SUPABASE_CONFIG = {
   url: 'https://kgpnzhyomrhfzljiipxj.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtncG56aHlvbXJoZnpsamlpcHhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMjIxNDYsImV4cCI6MjEwNDc5ODE0Nn0._9bIKaKhfNxFirKzsZ817CQ_MfcY7OM6s0_lgfWG3ZI',
 };
-window.SETTINGS_PIN = '4271';
+window.SETTINGS_PIN = '1102';
